af2959cfc6a7342e333b853816023ec3d9894f4d
Kozlov992 <55670841+Kozlov992@users.noreply.github.com>
2020-06-01 18:03:23 +0300

    Update Ball.hpp

--- Ball.hpp
+++ Ball.hpp
@@ -3,6 +3,11 @@
 #include "Block.hpp"
 #define _USE_MATH_DEFINES
 #include <math.h>
+static sf::Color ACC_MODE1(255, 46, 18);
+static sf::Color ACC_MODE2(174, 17, 61);
+static sf::Color ACC_MODE3(176, 30, 0);
+static sf::Color ACC_MODE4(78, 0, 0);
+static sf::Color ACCELERATION_MODES[4] = { ACC_MODE1 , ACC_MODE2 , ACC_MODE3, ACC_MODE4 };
 class Ball : public GameObjectCircle {
 	double _angle;
 	double _velocity;
